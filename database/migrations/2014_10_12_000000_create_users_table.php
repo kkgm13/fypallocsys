8530fa265bccce27c27aeae857d3b4f90eef4152
Kel Ming <kkgm013@gmail.com>
2019-10-03 13:06:27 +0100

    Auth Scaffolding Ready and Starting Models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('name')->comment("User's Name");
+            $table->string('username')->comment("User's Aston username");
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            // $table->timestamp('email_verified_at')->nullable(); // Not needed
+            // $table->string('password'); // Check with getting the UNIVERSITY DETAILS
             $table->rememberToken();
             $table->timestamps();
         });
