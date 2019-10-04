1425dccf436fff1b2756c1b0c378674b1c0ebd0e
Kel Ming <kkgm013@gmail.com>
2019-10-04 07:37:03 +0100

    Migration Details

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name')->comment("User's Name");
-            $table->string('username')->comment("User's Aston username");
+            $table->string('name')->comment("Aston User's Name");
+            $table->string('username')->comment("Aston User's username");
             $table->string('email')->unique();
+            $table->string('programme')->nullable()->comment('Aston Student Programme');
+            $table->enum('role', ['Student', 'Supervisor', 'Module Leader', 'Admin'])->comment('Roles Mechanisms')->default('Student');
+
             // $table->timestamp('email_verified_at')->nullable(); // Not needed
             // $table->string('password'); // Check with getting the UNIVERSITY DETAILS
             $table->rememberToken();
-            $table->timestamps();
+            // $table->timestamps();
         });
     }
 
--- database/migrations/2019_10_03_120312_create_topics_table.php
+++ database/migrations/2019_10_03_120312_create_topics_table.php
@@ -15,7 +15,14 @@ class CreateTopicsTable extends Migration
     {
         Schema::create('topics', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name')->comment('Project Name');
+            $table->longText('description')->comment('Project Description');
+            $table->unsignedInteger('userID')->comment('Foreign Key to USERS');
+            $table->boolean('isMCApprove')->comment('Boolean: CS Multimedia Approved');
+            $table->boolean('isCBApprove')->comment('Boolean: CS Business Approved');
+            
             $table->timestamps();
+            $table->foreign('userID')->references('id')->on('users');
         });
     }
 
