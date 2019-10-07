94250c33d00cfe6c8da228928f7b67b652a08b0b
Kel Ming <kkgm013@gmail.com>
2019-10-07 17:08:27 +0100

    Topic stuff starting

--- app/Topic.php
+++ app/Topic.php
@@ -4,7 +4,19 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Topic Model
+ * 
+ * This is the Model that provides context with a 
+ */
 class Topic extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'description', 'userID',
+    ];
 }
