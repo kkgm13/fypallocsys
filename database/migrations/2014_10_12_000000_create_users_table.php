8072876fbc83687e9a3f3aca2d259f6723d7c239
Kel Ming <kkgm013@gmail.com>
2019-10-17 14:25:58 +0100

    Navigation bar starting

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ class CreateUsersTable extends Migration
             $table->string('programme')->nullable()->comment('Aston Student Programme');
             $table->enum('role', ['Student', 'Supervisor', 'Module Leader', 'Admin'])->comment('Roles Mechanisms')->default('Student');
             $table->integer('maxProject')->comment('Maximum Project Selection')->default(0); // Limit via Saving and merging
-            // $table->timestamp('email_verified_at')->nullable(); // Not needed
+            $table->timestamp('email_verified_at')->nullable(); // Not needed
             $table->string('password'); // Check with getting the UNIVERSITY DETAILS
             $table->rememberToken();
             $table->timestamps();
